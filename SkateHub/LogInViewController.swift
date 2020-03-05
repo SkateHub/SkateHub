2a1257b5181e0d52aabfd47713aaf83c46b18a7d
Jose Patino <jose.patino88@icloud.com>
2020-03-05 11:34:39 -0500

    Basic Structure

---
+++ SkateHub/LogInViewController.swift
@@ -0,0 +1,30 @@
+//
+//  logInViewController.swift
+//  SkateHub
+//
+//  Created by Jose Patino on 3/5/20.
+//  Copyright © 2020 Jose Patino. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
