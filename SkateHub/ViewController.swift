ef08654fb3982e0d2c37f7212cca00787be33913
Jose Patino <jose.patino88@icloud.com>
2020-03-05 11:12:41 -0500

    Initial Commit

---
+++ SkateHub/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkateHub
+//
+//  Created by Jose Patino on 3/5/20.
+//  Copyright © 2020 Jose Patino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
