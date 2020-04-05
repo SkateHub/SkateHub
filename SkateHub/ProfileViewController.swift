68470979f9fcac182b7a15f5a6ac28d43654b31b
Aldo <aalmeidarojo18@students.desu.edu>
2020-04-05 11:51:58 -0400

    Improving user experience

--- SkateHub/ProfileViewController.swift
+++ SkateHub/ProfileViewController.swift
@@ -14,11 +14,28 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
 
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var bioLabel: UITextView!
+    @IBOutlet weak var bgImage: UIImageView!
+    @IBOutlet weak var updateBtn: UIButton!
+    @IBOutlet weak var logBtn: UIButton!
+    @IBOutlet weak var deleteBtn: UIButton!
+    var isProfilePic:Bool=true
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        profileImage.layer.cornerRadius=profileImage.frame.height/2
+        profileImage.clipsToBounds=true
+        profileImage.layer.borderColor=UIColor.systemPink.cgColor
+        profileImage.layer.borderWidth=2
+        updateBtn.layer.cornerRadius=6
+        logBtn.layer.cornerRadius=6
+        deleteBtn.layer.cornerRadius=6
+        bioLabel.layer.cornerRadius=2
+        bioLabel.layer.borderColor=UIColor.systemPink.cgColor
+        bioLabel.layer.borderWidth=0.5
         updateProfileImage()
+        updateBgImage()
         bioLabel.text=PFUser.current()!["bio"] as! String
+        self.view.layoutIfNeeded()
         
     }
     
@@ -31,6 +48,17 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         }
     }
     
+    func updateBgImage(){
+        if let user = PFUser.current(){
+            if user["bgImage"] != nil{
+                let image=user["bgImage"] as! PFFileObject
+                guard let urlString=image.url else { return }
+                let url=URL(string: urlString)!
+                bgImage.af_setImage(withURL: url)
+            }
+        }
+    }
+    
     @IBAction func onLogout(_ sender: Any) {
         PFUser.logOut()
         let main=UIStoryboard(name: "Main", bundle: nil)
@@ -38,28 +66,47 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         let scene=self.view.window?.windowScene?.delegate  as! SceneDelegate
         scene.window?.rootViewController=login
     }
+    @IBAction func onEditBG(_ sender: Any) {
+        isProfilePic=false
+        imagePicker()
+    }
     @IBAction func onEditImage(_ sender: Any) {
+        isProfilePic=true
+        imagePicker()
+    }
+    
+    func imagePicker(){
         let picker = UIImagePickerController()
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true, completion: nil)
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
         var currentImage: UIImage!
         let image = info[.editedImage] as! UIImage
         currentImage = image
         
         if let user = PFUser.current(){
-            let profileData=currentImage.pngData()
-            let imageFile=PFFileObject(name: "profileImage.png", data: profileData!)
-            user["profileImage"]=imageFile
-            user.saveInBackground()
-            dismiss(animated: true, completion: nil)
-            let alert=UIAlertController(title: "Profile Image Updated", message: "Exit and press profile again to see changes!", preferredStyle: UIAlertController.Style.alert)
-            let alertAction=alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            
+            if(isProfilePic){
+                let profileData=currentImage.pngData()
+                let imageFile=PFFileObject(name: "profileImage.png", data: profileData!)
+                user["profileImage"]=imageFile
+                user.saveInBackground()
+                dismiss(animated: true, completion: nil)
+                let alert=UIAlertController(title: "Profile Image Updated", message: "Go back and press profile again to see changes!", preferredStyle: UIAlertController.Style.alert)
+                let alertAction=alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            } else{
+                let profileData=currentImage.pngData()
+                let imageFile=PFFileObject(name: "bg.png", data: profileData!)
+                user["bgImage"]=imageFile
+                user.saveInBackground()
+                dismiss(animated: true, completion: nil)
+                let alert=UIAlertController(title: "Background Updated", message: "Go back and press profile again to see changes!", preferredStyle: UIAlertController.Style.alert)
+                let alertAction=alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
         }
     }
     @IBAction func onUpdateBio(_ sender: Any) {
@@ -72,6 +119,15 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
             let alert=UIAlertController(title: "Shorten bio", message: "Bio must be less than 30 characters!", preferredStyle: UIAlertController.Style.alert)
             let alertAction=alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
             self.present(alert, animated: true, completion: nil)
+        }
+    }
+    @IBAction func onDeleteAccount(_ sender: Any) {
+        if let user=PFUser.current(){
+            user.deleteInBackground()
+            let main=UIStoryboard(name: "Main", bundle: nil)
+            let login=main.instantiateViewController(withIdentifier: "loginView")
+            let scene=self.view.window?.windowScene?.delegate  as! SceneDelegate
+            scene.window?.rootViewController=login
         }
     }
     /*
