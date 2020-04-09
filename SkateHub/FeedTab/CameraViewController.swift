3af0102292387410a9fa5880969dc15d74b05d3d
Paola <camachopaola311@gmail.com>
2020-04-09 00:16:01 -0400

    Can create post/photo with caption that saves to Parse

--- SkateHub/FeedTab/CameraViewController.swift
+++ SkateHub/FeedTab/CameraViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Parse
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
@@ -20,6 +20,27 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onPostButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = captionField.text!
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success{
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!")
+            }else{
+                print("Error!")
+            }
+            
+            
+            
+        }
         
     }
     
@@ -34,6 +55,20 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
             picker.sourceType = .photoLibrary
         }
         present(picker, animated: true, completion: nil)
+        
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+        
+        
+        
         
         
     }
