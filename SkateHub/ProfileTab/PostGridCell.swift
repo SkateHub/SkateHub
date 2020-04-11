850b1ada38551c0c3c2a2c204e649e9176328a8f
Jose Patino <jose.patino88@icloud.com>
2020-04-11 13:24:04 -0400

    Just need photos to show in the cells but mostly done

---
+++ SkateHub/ProfileTab/PostGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  PostGridCell.swift
+//  SkateHub
+//
+//  Created by Jose Patino on 4/11/20.
+//  Copyright © 2020 Jose Patino. All rights reserved.
+//
+
+import UIKit
+
+class PostGridCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var postPicture: UIImageView!
+}
--- SkateHub/ProfileTab/ProfileFeedViewController.swift
+++ SkateHub/ProfileTab/ProfileFeedViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 import AlamofireImage
 import Parse
 
-class ProfileFeedViewController: UIViewController
+class ProfileFeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate
 {
 
+    var posts = [PFObject]()
+    
     @IBOutlet weak var nameOfUserLabel: UILabel!
     @IBOutlet weak var bioLabel: UILabel!
     @IBOutlet weak var profilePictureImageView: UIImageView!
     @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var postCollectionView: UICollectionView!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        postCollectionView.delegate = self
+        postCollectionView.dataSource = self
         
         if let user = PFUser.current()
         {
@@ -37,11 +43,53 @@ class ProfileFeedViewController: UIViewController
             let userFirstName = user["FirstName"] as! String
             let userLastName = user["LastName"] as! String
             nameOfUserLabel.text = "\(userFirstName) \(userLastName)"
+            
+            
         }
         reloadInputViews()
+        self.postCollectionView.reloadData()
     }
     
+    override func viewDidAppear(_ animated: Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Posts")
+        query.includeKeys(["author", "image"])
+        query.limit = 20
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil
+            {
+                self.posts = posts!
+                self.postCollectionView.reloadData()
+            }
+        }
+    }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return posts.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
+    {
+        let post = posts[indexPath.section]
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostGridCell", for: indexPath) as! PostGridCell
+        let user = post["author"] as! PFUser
+        let currentUser = PFUser.current()
+        
+        if user == currentUser
+        {
+            let postPicture = post["image"] as! PFFileObject
+            let urlString  =  postPicture.url!
+            let url = URL(string: urlString)!
+            cell.postPicture.af_setImage(withURL: url)
+            
+        }
+        
+        return cell
+    }
+    
+
 
 }
